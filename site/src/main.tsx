4b00311b32ce1e42ef87e5bc0f7763a20fbb7da6
TySteamTest <130309211+TySteamTest@users.noreply.github.com>
2025-12-26 05:32:19 -0500

    chore: trigger Deploy Frontend workflow

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 
 // Cognito callback URL fix deployed to support root domain
+// Workflow fixed to only deploy frontend
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
