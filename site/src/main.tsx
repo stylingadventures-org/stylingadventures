c4287504370ade104f360fd6baedd224b2c2fced
TySteamTest <130309211+TySteamTest@users.noreply.github.com>
2025-12-26 05:40:24 -0500

    chore: deploy with react-router-dom fix

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -6,6 +6,7 @@ import './index.css';
 // Cognito callback URL fix deployed to support root domain
 // Workflow fixed to only deploy frontend
 // Fixed npm cache configuration
+// Fixed react-router-dom dependency
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
